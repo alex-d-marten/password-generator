71973436907999020de05d6915d8c41cda4addd1
amarten <alexdmarten@gmail.com>
2021-11-19 19:39:24 -0800

    lowercase random generation and printing

--- Develop/script.js
+++ Develop/script.js
@@ -2,12 +2,14 @@
 // declare variables and constants
 var minLength = 8;
 var maxLength = 128;
-var alphabet = "abcdefghijklmnopqrstuvwxyz";
+var password = [];
+// adding a space in the beginning makes the first element of the array blank which is helpful for random selection of all array elements
+var alphabet = " abcdefghijklmnopqrstuvwxyz";
 var lowerCaseLetters = alphabet.split('');
-let upperCaseLetters = alphabet.toUpperCase();
+var upperCaseLetters = alphabet.toUpperCase();
 upperCaseLetters = upperCaseLetters.split('');
-var numbers = '123456789'.split('');
-var specialCharacters = "'`~!@#$%^&*()_-=+<>,./?[]{}\|;:".split('');
+var numbers = ' 123456789'.split('');
+var specialCharacters = " '`~!@#$%^&*()_-=+<>,./?[]{}\|;:".split('');
 
 var inputPasswordLength = function() {
   var passwordLength = window.prompt("How long would you like your password to be? Please enter between 8 and 128 characters.");
@@ -47,6 +49,7 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  var password = [];
   // run prompt functions for user input
   inputPasswordLength();
   inputCharacters();
@@ -60,28 +63,48 @@ function writePassword() {
 
   // password generation logic
   for (var i = 0; i < passwordLength; i++) {
-    randomNumArr = [];
-    randomNum = Math.random();
-    console.log(randomNum);
+    charSelector = 0.23;
+    // Math.random();
+    arrayMultiplier = Math.random();
+    // need to store the randomly selected characters and then make a string out of it somehow
+    // Array.join('') may work to combine an array to one continuous string
+    // Array.push() adds new element to array
 
-    if(desiredCharacters[1] && randomNum < 0.25) {
+    if(desiredCharacters[0] && charSelector < 0.25) {
       // generate a random lowercase value and store it in password
+      // var passwordCharacter;
+      var characterIndex = Math.floor(arrayMultiplier*lowerCaseLetters.length + 1);
+      var randomCharacter = lowerCaseLetters[characterIndex];
+      // debugger;
+      // console.log(passwordCharacter);
+      localStorage.setItem("randomCharacter", randomCharacter);
     }
-    else if(desiredCharacters[2] && randomNum > 0.25 && randomNum < 0.50) {
-      // generate a random uppercase value and store it in password
-    }
-    else if(desiredCharacters[3] && randomNum > 0.50 && randomNum < 0.75) {
-      // generate a random number from 1-9 and store it in password
-    }
-    else if (desiredCharacters[4] && randomNum > 0.75) {
-      // generate a random special character and store it in password
-    }
+
+    var randomCharacter = localStorage.getItem("randomCharacter");
+    password.push(randomCharacter);
+    passwordString = password.join('');
+
+    localStorage.setItem("passwordString", passwordString);
+    console.log(passwordText);
+    // else if(desiredCharacters[1] && charSelector > 0.25 && charSelector < 0.50) {
+    //   // generate a random uppercase value and store it in password
+    // }
+    // else if(desiredCharacters[2] && charSelector > 0.50 && charSelector < 0.75) {
+    //   // generate a random number from 1-9 and store it in password
+    // }
+    // else if (desiredCharacters[3] && charSelector > 0.75) {
+    //   // generate a random special character and store it in password
+    // }
   }
+  localStorage.removeItem("password");
+  // An idea for getting random values is to do some math. Whatever the value of the array is you can do Math.random * [The Array] + 1
+  // to get a random value then use Math.floor to get a whole number which can be used to access array
+  // one fallback is you won't ever hit the 0 value
+  // an option to resolve this is to make the 0 value nothing by adding a space
 
 
 
-
-
+  password = localStorage.getItem("passwordString");
   // var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
