3d5c07e305cb671e1be2e5aa8d8e493263c69669
amarten <alexdmarten@gmail.com>
2021-11-19 18:11:36 -0800

    adding variable arrays and random generator structure

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,13 @@
 // Assignment code here
+// declare variables and constants
 var minLength = 8;
 var maxLength = 128;
+var alphabet = "abcdefghijklmnopqrstuvwxyz";
+var lowerCaseLetters = alphabet.split('');
+let upperCaseLetters = alphabet.toUpperCase();
+upperCaseLetters = upperCaseLetters.split('');
+var numbers = '123456789'.split('');
+var specialCharacters = "'`~!@#$%^&*()_-=+<>,./?[]{}\|;:".split('');
 
 var inputPasswordLength = function() {
   var passwordLength = window.prompt("How long would you like your password to be? Please enter between 8 and 128 characters.");
@@ -23,25 +30,59 @@ var inputPasswordLength = function() {
   }
 
 }
-inputPasswordLength();
 
-// grab passwordLength from localStorage and store globally
-var passwordLength = localStorage.getItem("passwordLength");
-console.log(passwordLength);
+var inputCharacters = function() {
+  var desiredCharacters = [];
+  desiredCharacters[0] = window.confirm("Do you want LOWERCASE CHARACTERS in your password? Select 'Ok' if so, if you do not then select 'Cancel'");
+  desiredCharacters[1] = window.confirm("Do you want UPPERCASE CHARACTERS in your password? Select 'Ok' if so, if you do not then select 'Cancel'");
+  desiredCharacters[2] = window.confirm("Do you want NUMBERS in your password? Select 'Ok' if so, if you do not then select 'Cancel'");
+  desiredCharacters[3] = window.confirm("Do you want SPECIAL CHARACTERS? Select 'Ok' if so, if you do not then select 'Cancel'");
+  desiredCharacters = JSON.stringify(desiredCharacters);
+  localStorage.setItem("desiredCharacters", desiredCharacters);
 
-var userInput = function() {
-  if (userClick) {
-    var passwordLength = window.prompt("How long would you like your password to be? Please select between 8 and 128 characters.")
-  }
 }
-
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  // run prompt functions for user input
+  inputPasswordLength();
+  inputCharacters();
+
+  // retrieve users prompt answers from local storage
+  var passwordLength = localStorage.getItem("passwordLength");
+  var desiredCharacters = localStorage.getItem("desiredCharacters");
+
+  // convert desiredCharacters string back to an array
+  desiredCharacters = JSON.parse(desiredCharacters);
+
+  // password generation logic
+  for (var i = 0; i < passwordLength; i++) {
+    randomNumArr = [];
+    randomNum = Math.random();
+    console.log(randomNum);
+
+    if(desiredCharacters[1] && randomNum < 0.25) {
+      // generate a random lowercase value and store it in password
+    }
+    else if(desiredCharacters[2] && randomNum > 0.25 && randomNum < 0.50) {
+      // generate a random uppercase value and store it in password
+    }
+    else if(desiredCharacters[3] && randomNum > 0.50 && randomNum < 0.75) {
+      // generate a random number from 1-9 and store it in password
+    }
+    else if (desiredCharacters[4] && randomNum > 0.75) {
+      // generate a random special character and store it in password
+    }
+  }
+
+
+
+
+
+  // var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
